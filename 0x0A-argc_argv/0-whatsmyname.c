b5af1e71f998aa89c5903f6c665fcddbae0102c0
ernest <episani90@gmail.com>
2022-11-09 22:40:20 +0300

    argc,argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 /**
-* Description - Uses of argc and argv
-* @main: a program that prints its name
-* @argc: Print with new name upon  renaming
-* @argv: Path to be present
-* Return: Always 0 (Success)
+* main - prints its name + \n
+* @argc: count
+* @argv: print name
+* Return: program name
 */
 int main(int argc, char *argv[])
 {
 int i;
-for (i =0; i < argc;i++)
+for (i = 0 ; i < argc; i++)
 {
 printf("%s ", argv[i]);
 }
