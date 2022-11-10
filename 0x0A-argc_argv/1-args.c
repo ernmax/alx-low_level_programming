276aa3519c298fb7013f8caa2a7ae77fef304cdd
ernest <episani90@gmail.com>
2022-11-10 18:25:52 +0300

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,6 +11,6 @@ int main(int argc, char *argv[])
 {
 int counter;
 for (counter = 0; counter < argc ; counter++)
-printf("argv[%2d]: %s\n", counter, argv[counter]);
+printf("%d: %s\n", counter, argv[counter]);
 return (0);
 }
